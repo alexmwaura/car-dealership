ce6f3ebe93787c1e659f3c0028deff6b4e968390
alexmwaura <alexmwaura43@gmail.com>
2020-10-20 13:15:44 +0400

    add test case for geting model

--- src/test/java/VehicleTest.java
+++ src/test/java/VehicleTest.java
@@ -1,5 +1,18 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class VehicleTest {
+   ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>();
+   Vehicle suv = new Vehicle(2002, "Ford", "Explorer",100000, 7000);
+    @Test
+   public void getVehicle_shouldGetVehicleModel_String(){
+       vehicles.add(suv);
+       for (Vehicle vehicle: vehicles){
+            assertEquals(vehicle.getModel(),"Explorer");
+       }
 
+   }
 }
